d7c5e780408a84e57338508bd2050e355fb30189
Alexandre Pereira <alexandre.carneiro.pereira@gmail.com>
2022-10-04 12:45:00 +0100

    add main commands

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Running")
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.DB_STRING)
+        console.log(`✅ MongoDB connected at ${conn.connection.host}`)
+    } catch (err) {
+        console.log(err)
+    }
+}
+connectDB()
+
+app.use(express.static("public"))
+
+// app.use() mainRoutes
+// app.use() api Routes
+
+app.listen(process.env.PORT, () => {
+    console.log(`✅ Express connected on port ${process.env.PORT}`)
+})
