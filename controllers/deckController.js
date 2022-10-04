8afdfc50ccca160fe08f617be953ea44f2554dd2
Alexandre Pereira <alexandre.carneiro.pereira@gmail.com>
2022-10-04 17:04:48 +0100

    Add shuffle feature to new deck

--- controllers/deckController.js
+++ controllers/deckController.js
@@ -8,6 +8,22 @@ const JOKERS = ['X1', 'X2']
 const SUITS = { S: 'SPADES', D: 'DIAMONDS', H: 'HEARTS', C: 'CLUBS' } // , 1: 'BLACK', 2: 'RED'
 const VALUES = { A: 'ACE', J: 'JACK', Q: 'QUEEN', K: 'KING', 0: '10', X: 'JOKER' }
 
+let shuffle = arr => {
+    // Fisher–Yates shuffle
+    let a = [...arr]
+    let i = arr.length - 1
+    let tmp
+    while (i >= 0) {
+        r = Math.floor(Math.random() * a.length)
+        tmp = a[r]
+        a[r] = a[i]
+        a[i] = tmp
+        i -= 1
+    }
+    return a
+}
+
+
 exports.newDeck = (req, res) => {
     const newDeck = new Deck({ stack: JSON.stringify(DEFAULT_CARDS), deckContent: JSON.stringify(DEFAULT_CARDS), remaining: DEFAULT_CARDS.length, shuffled: false, includeJokers: false })
     newDeck.save((err, success) => {
@@ -17,3 +33,11 @@ exports.newDeck = (req, res) => {
     })
 }
 
+exports.newDeckShuffled = (req, res) => {
+    const newDeck = new Deck({ stack: JSON.stringify(shuffle(DEFAULT_CARDS)), deckContent: JSON.stringify(DEFAULT_CARDS), remaining: DEFAULT_CARDS.length, shuffled: true, includeJokers: false })
+    newDeck.save((err, success) => {
+        if (err) { console.log(err); res.status(500).json("Error") }
+        const { shuffled, remaining, _id } = success
+        res.json({ success: true, deck_id: _id, remaining, shuffled })
+    })
+}
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router()
 const deckController = require("../controllers/deckController")
 
 router.get("/new", deckController.newDeck)
-// router.get("/new/shuffle", deckController.newDeckShuffled)
+router.get("/new/shuffle", deckController.newDeckShuffled)
 
 module.exports = router
